6d13cc1028b2fd394fe632ded90d626931dd3067
Josef Weinbub <weinbub@iue.tuwien.ac.at>
2014-02-04 08:54:25 +0100

    parallel build and tests for jwein2

--- jwein2/ctestscript-gcc-debug.cmake
+++ jwein2/ctestscript-gcc-debug.cmake
@@ -4,10 +4,12 @@ set(CTEST_SOURCE_DIRECTORY "${ROOT_DIRECTORY}/viennafem-dev")
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 set(CTEST_BUILD_CONFIGURATION "Debug")
 set(CTEST_BUILD_OPTIONS "")
-SET(ENV{CC} /usr/bin/gcc)
-SET(ENV{CXX} /usr/bin/g++)
+set(ENV{CC} /usr/bin/gcc)
+set(ENV{CXX} /usr/bin/g++)
 set(COMPILER_NAME "GCC")
 set(COMPILER_VERSION "4.8.1")
+set(CTEST_BUILD_FLAGS -j4) # build tests in parallel
+set(CTEST_COMMAND "${CTEST_EXECUTABLE_NAME} -j4") # run tests in parallel
 set(WITH_MEMCHECK FALSE)
 set(WITH_COVERAGE FALSE)
 
