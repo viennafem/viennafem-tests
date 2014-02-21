58ce85ace50e093141eb38a6e7624b4f8cc7d463
Karl Rupp <rupp@iue.tuwien.ac.at>
2014-02-21 16:49:31 +0100

    Added Windows XP machine (hosted at IuE).

---
+++ winxp/ctestscript-visual-studio-10-release.cmake
@@ -0,0 +1,13 @@
+
+set(ROOT_DIRECTORY "D:/git/ViennaFEM")
+set(CTEST_SOURCE_DIRECTORY "${ROOT_DIRECTORY}/viennafem-dev")
+set(CTEST_CMAKE_GENERATOR "Visual Studio 10")
+set(CTEST_BUILD_CONFIGURATION "Release")
+set(CTEST_BUILD_OPTIONS "")
+set(COMPILER_NAME "Visual Studio")
+set(COMPILER_VERSION "10.2010")
+set(WITH_MEMCHECK FALSE)
+set(WITH_COVERAGE FALSE)
+
+########################################################################
+include(${ROOT_DIRECTORY}/viennafem-tests/ctestscript-common.cmake)
---
+++ winxp/ctestscript-visual-studio-8-debug.cmake
@@ -0,0 +1,13 @@
+
+set(ROOT_DIRECTORY "D:/git/ViennaFEM")
+set(CTEST_SOURCE_DIRECTORY "${ROOT_DIRECTORY}/viennafem-dev")
+set(CTEST_CMAKE_GENERATOR "Visual Studio 8 2005")
+set(CTEST_BUILD_CONFIGURATION "Debug")
+set(CTEST_BUILD_OPTIONS "")
+set(COMPILER_NAME "Visual Studio")
+set(COMPILER_VERSION "8.2005")
+set(WITH_MEMCHECK FALSE)
+set(WITH_COVERAGE FALSE)
+
+########################################################################
+include(${ROOT_DIRECTORY}/viennafem-tests/ctestscript-common.cmake)
